1816bc3d3172f42a65480279a4eaecf00de155e1
ameer alabi <ameeralabi7@gmail.com>
2025-02-20 23:08:23 +0100

    edit

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors({
 }));
 
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/airtime_to_cash";
+const MONGO_URI = process.env.MONGO_URI;
 
 mongoose
   .connect(MONGO_URI,)
@@ -29,6 +29,7 @@ const transactionSchema = new mongoose.Schema({
   bankName: String,
   accountNumber: String,
 });
+
 
 const Transaction = mongoose.model("Transaction", transactionSchema);
 
