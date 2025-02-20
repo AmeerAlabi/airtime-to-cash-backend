bf8a8765ef1aa3e8e6055930688da24ecb27e8ba
ameer alabi <ameeralabi7@gmail.com>
2025-02-20 22:40:48 +0100

    edit

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ require("dotenv").config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "https://airtime-to-cash-frontend.vercel.app",
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization"
+}));
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/airtime_to_cash";
